577ff3ee1dd9c55cde10335b4ce8b91faccccfb2
pooria98 <pooriakheyri@yahoo.com>
2025-10-01 19:06:28 +0330

    add placeholder settings page

---
+++ app/user/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="w-full h-[calc(100dvh-70px)] flex justify-center items-center">
+      This page is a work in progress...
+    </div>
+  );
+};
+
+export default Page;
